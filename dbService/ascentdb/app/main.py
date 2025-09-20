755cc52a40cada2640e1bbbd4259449c66134c50
pranav5127 <pranav.kumar.5127@gmail.com>
2025-09-20 17:46:19 +0530

    update: dir structure

--- dbService/ascentdb/app/main.py
+++ dbService/ascentdb/app/main.py
@@ -14,10 +14,10 @@ app.include_router(activities.router)
 app.include_router(reports.router)
 app.include_router(users.router)
 
-if __name__ == "__main__":
-    uvicorn.run(
-        "ascentdb.app.main:app",
-        host="0.0.0.0",
-        port=9900,
-        reload=True
-    )
+# if __name__ == "__main__":
+#     uvicorn.run(
+#         "ascentdb.app.main:app",
+#         host="0.0.0.0",
+#         port=9900,
+#         reload=True
+#     )
